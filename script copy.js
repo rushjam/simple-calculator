740419bbd22c85f2d0edd7c25d1eeb0e3ce70117
Rushik Ramani <rush.ramani@gmail.com>
2021-10-01 15:45:56 -0700

    file added

--- script copy.js
+++ script copy.js
@@ -1,20 +1,4 @@
-function getHistory() {
-  return document.getElementById("history-value").innerText;
-}
 
-function printHistory(num) {
-  document.getElementById("history-value").innerText = num;
-}
-function getOutput() {
-  return document.getElementById("output-value").innerText;
-}
-function printOutput(num) {
-  if (num == "") {
-    document.getElementById("output-value").innerText = num;
-  } else {
-    document.getElementById("output-value").innerText = getFormattedNumber(num);
-  }
-}
 function getFormattedNumber(num) {
   if (num == "-") {
     return "";
@@ -52,25 +36,9 @@ for (var i = 0; i < operator.length; i++) {
         history = history + output;
         if (this.id == "=") {
           var result = eval(history);
-          printOutput(result);
-          printHistory("");
-        } else {
-          history = history + this.id;
-          printHistory(history);
-          printOutput("");
+          
         }
       }
-    }
-  });
-}
-var numbers = document.getElementsByClassName("number");
-for (var i = 0; i < number.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    var output = reverseNumberFormat(getOutput());
-    if (output != NaN) {
-      //if output is a number
-      output = output + this.id;
-      printOutput(output);
     }
   });
 }
